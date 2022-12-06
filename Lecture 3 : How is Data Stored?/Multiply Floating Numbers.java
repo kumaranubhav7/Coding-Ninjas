63f4ffe68b62c7fa2a714478954a57ecc3d7a960
Anubhav Kumar <97359995+kumaranubhav7@users.noreply.github.com>
2022-12-06 22:26:31 +0530

    Create Multiply Floating Numbers.java

---
+++ Lecture 3 : How is Data Stored?/Multiply Floating Numbers.java
@@ -0,0 +1,10 @@
+/*Given two floating numbers, ‘F1’ and ‘F2’, write a program to return the product of these floating numbers.
+Give your answer to 6 decimal places.*/
+
+public class Solution {
+    public static double multiplyFloatingNumbers(double f1, double f2) {
+        // Write your code here
+        double f3 = f1*f2;
+        return f3;
+    }
+}
