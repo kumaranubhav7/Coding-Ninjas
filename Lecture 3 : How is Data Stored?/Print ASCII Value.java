a70e49b24c7db97fb8376e77fdf0541491f2b875
Anubhav Kumar <97359995+kumaranubhav7@users.noreply.github.com>
2022-12-06 22:28:14 +0530

    Create Print ASCII Value.java

---
+++ Lecture 3 : How is Data Stored?/Print ASCII Value.java
@@ -0,0 +1,8 @@
+/*Given a character, you need to return the ASCII value of that character.*/
+
+public class Solution {
+    public static int calculateASCII(char ch) {
+        // Write your code here
+        return ch;
+    }
+}
